bba293426f507810c5bb8a84b5f7e19185b09c44
Rafael Mule <rafael-bm@protonmail.com>
2020-05-20 17:15:55 +0100

    Fix lambda variable for iam policy

--- module/lambda/main.tf
+++ module/lambda/main.tf
@@ -13,7 +13,6 @@ resource "aws_lambda_function" "main" {
   }
 }
 
-
 resource "aws_iam_policy_attachment" "main" {
   name = var.name
   policy_arn = aws_iam_policy.main.arn
@@ -46,28 +45,5 @@ EOF
 
 resource "aws_iam_policy" "main" {
   name = var.name
-  policy = var.policy
-//  policy = <<EOF
-//{
-//    "Version": "2012-10-17",
-//    "Statement": [
-//        {
-//            "Effect": "Allow",
-//            "Action": [
-//                "logs:CreateLogGroup",
-//                "logs:CreateLogStream",
-//                "logs:PutLogEvents"
-//            ],
-//            "Resource": "*"
-//        },
-//        {
-//          "Effect": "Allow",
-//          "Action": [
-//            "secretsmanager:GetSecretValue"
-//          ],
-//          "Resource": "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${local.secret_name}*"
-//        }
-//    ]
-//}
-//EOF
+  policy = var.iam_policy
 }
