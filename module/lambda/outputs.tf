ad37794d192a1704e7f80336d48ec24aff3eba06
Rafael Mule <rafael-bm@protonmail.com>
2020-11-17 16:40:57 +0000

    Add invoke_arn output to lambda modules

--- module/lambda/outputs.tf
+++ module/lambda/outputs.tf
@@ -6,6 +6,10 @@ output "lambda_arn" {
   value = aws_lambda_function.main.arn
 }
 
+output "lambda_invoke_arn" {
+  value = aws_lambda_function.main.invoke_arn
+}
+
 output "lambda_function_name" {
   value = aws_lambda_function.main.function_name
 }
--- module/lambda_trigger/outputs.tf
+++ module/lambda_trigger/outputs.tf
@@ -6,6 +6,10 @@ output "lambda_arn" {
   value = module.lambda.lambda_arn
 }
 
+output "lambda_invoke_arn" {
+  value = module.lambda.invoke_arn
+}
+
 output "lambda_function_name" {
   value = module.lambda.lambda_function_name
 }
