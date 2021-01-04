05d8e8738c80b73a8fb53915e818dd5d02c16e45
Rafael Mule <rafael-bm@protonmail.com>
2021-01-04 13:28:32 +0000

    Update RDS module

--- module/rds_postgres/main.tf
+++ module/rds_postgres/main.tf
@@ -45,7 +45,8 @@ resource "aws_db_instance" "db" {
   username = random_password.username.result
   password = random_password.password.result
   backup_retention_period = var.backup_retention_period
-  deletion_protection = true
+  deletion_protection = false
+  auto_minor_version_upgrade = false
   storage_encrypted = false
   skip_final_snapshot = false
   final_snapshot_identifier = "${var.db_identifier}-SNAPSHOT"
